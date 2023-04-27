c93ad6388d1fdfeb1f4b2d747a436ec6cf1282de
= <=>
2023-04-27 22:21:30 +0800

    第一次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -7,14 +7,19 @@ try:
 except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
-
+# 岭回归
 def ridge(data):
-    pass
-    
+    x, y = read_data()
+    # w=(X^T X)^-1 (X^T y)
+    weight = np.matmul((np.linalg.inv(np.matmul(X.T, X)), np.matmul(X.T,y)))
+    return weight @ data
+# lasso回归
 def lasso(data):
-    pass
+    return ridge(data)
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
     return x, y
+
+read_data('../data/exp02/')
