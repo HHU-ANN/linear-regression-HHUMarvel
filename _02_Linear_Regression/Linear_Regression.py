1b70f1680435ee1589a4dcc423df63131f026ce7
= <=>
2023-04-28 11:59:55 +0800

    第二次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -9,17 +9,35 @@ except ImportError as e:
     import numpy as np
 # 岭回归
 def ridge(data):
-    x, y = read_data()
-    # w=(X^T X)^-1 (X^T y)
-    weight = np.matmul((np.linalg.inv(np.matmul(X.T, X)), np.matmul(X.T,y)))
-    return weight @ data
+    # 读入数据
+    X, y = read_data()
+    # 超参数
+    alpha = 0.1
+    # 正则化项
+    A = alpha * np.eye(X.shape[1])# alpha * I
+    # 最小二乘求权重 w = (X^T X + A)^-1 (X^T y)
+    w = np.matmul((np.linalg.inv(np.matmul(X.T, X) + A), np.matmul(X.T,y)))
+    return w @ data
 # lasso回归
 def lasso(data):
-    return ridge(data)
+    # 读入数据
+    X, y = read_data()
+    # 超参数
+    alpha = 0.1
+    epoch = 1000
+    # 初始化w
+    w = np.zeros(X.shape[1])
+    # 梯度下降
+    for i in range(epoch):
+        # 梯度
+        grad = np.matmul(X.T, np.matmul(X, w) - y) / X.shape[0] + alpha * np.sign(w)
+        # 更新w
+        w = w - grad
+    return w @ data
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
     return x, y
 
-read_data('../data/exp02/')
+# read_data('../data/exp02/')
